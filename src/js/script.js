2f0be17a2ce2369634b70fc04f18a4dd159b7561
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-01-24 20:10:48 -0300

    :up: Configurando Navbar

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function aparecer(){
+    var menu = document.getElementById("navbar");
+    if(menu.style.display == "none"){
+        menu.style.display = "block";
+    }else{
+        menu.style.display = "none";
+    }
+}
